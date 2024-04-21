237ca8d483001f300d71018048b54580c3035a14
Konstantin Weitz <konstantin@financialchoice.com>
2024-04-20 17:12:28 -0700

    i can open the roof

--- slk.ino
+++ slk.ino
@@ -6,36 +6,23 @@
 const int SPI_CS_PIN = 9;
 mcp2515_can CAN(SPI_CS_PIN);
 
-bool IS_BASELINE_ID[0];
+bool IS_BASELINE_ID[2048];
 
-// unsigned long BASELINE_IDS[] = {
-//     0, 2, 3, 4, 6, 8, 10, 11, 12, 18,
-//     20, 22, 23, 24, 25, 30, 31, 44, 48, 56,
-//     88, 112, 144, 154, 156, 158, 178, 241, 384, 396,
-//     404, 416, 417, 420, 428, 434, 438, 444, 464, 528,
-//     546, 576, 624, 648, 676, 716, 724, 728, 767, 774,
-//     818, 992, 993, 1024, 1026, 1027, 1028, 1029, 1031, 1032,
-//     1033, 1034, 1036, 1037, 1041, 1044, 1045, 1048, 1051, 1052,
-//     1062, 1890, 1986, 1987, 2012
-// };
+unsigned long BASELINE_IDS[] = {
+    0, 2, 3, 4, 6, 8, 10, 11, 12, 18,
+    20, 22, 23, 24, 25, 30, 31, 44, 48, 56,
+    88, 112, 144, 154, 156, 158, 178, 241, 384, 396,
+    404, 416, 417, 420, 428, 434, 438, 444, 464, 528,
+    546, 576, 624, 648, 676, 716, 724, 728, 767, 774,
+    818, 992, 993, 1024, 1026, 1027, 1028, 1029, 1031, 1032,
+    1033, 1034, 1036, 1037, 1041, 1044, 1045, 1048, 1051, 1052,
+    1062, 1890, 1986, 1987, 2012
+};
 
-// void setupBaseline() {
-//   for (int i = 0; i < sizeof(BASELINE_IDS) / sizeof(unsigned long); ++i) {
-//     IS_BASELINE_ID[BASELINE_IDS[i]] = true;
-//   }
-// }
-
-void setup() {
-  // setupBaseline();
-
-  SERIAL.begin(115200);
-  while (!Serial) {};
-
-  while (CAN_OK != CAN.begin(CAN_83K3BPS, MCP_16MHz)) {
-    SERIAL.println("CAN BUS Shield init fail");
-    delay(1000);
+void setupBaseline() {
+  for (int i = 0; i < sizeof(BASELINE_IDS) / sizeof(unsigned long); ++i) {
+    IS_BASELINE_ID[BASELINE_IDS[i]] = true;
   }
-  SERIAL.println("CAN BUS Shield init ok!");
 }
 
 typedef struct {
@@ -45,8 +32,44 @@ typedef struct {
   unsigned char data[8];
 } frame_t;
 
-frame_t FRAMES[] = {
-};
+void printAsStruct(unsigned long id, byte len, byte* data) {
+  SERIAL.print("{.time=");
+  SERIAL.print(millis());
+  SERIAL.print(", .id=0x");
+  SERIAL.print(id, 16);
+  SERIAL.print(", .len=");
+  SERIAL.print(len);
+  SERIAL.print(", .data={");
+  for (int i = 0; i < len; ++i) {
+    if (i != 0) SERIAL.print(", ");
+    SERIAL.print("0x");
+    SERIAL.print(data[i], 16);
+  }
+  SERIAL.print("}},\n");
+}
+
+void printSimply(unsigned long id, byte len, byte* data) {
+  SERIAL.print(id);
+  SERIAL.print(":");
+  for (int i = 0; i < len; ++i) {
+    SERIAL.print(" ");
+    SERIAL.print(data[i], 16);
+  }
+  SERIAL.print("\n");
+}
+
+void receive() {
+  if (CAN.checkReceive() != CAN_MSGAVAIL) return;
+  byte len = 0;
+  byte data[8];
+  CAN.readMsgBuf(&len, data);
+  unsigned long id = CAN.getCanId();
+
+  if (IS_BASELINE_ID[id]) return;
+  
+  // printAsStruct(id, len, data);
+  printAsStruct(id, len, data);
+}
 
 frame_t REPLAY[] = {
   {.time=6323, .id=0x350, .len=1, .data={0x40}},
@@ -476,46 +499,6 @@ frame_t REPLAY[] = {
   {.time=10859, .id=0x10, .len=1, .data={0x30}},
 };
 
-void printAsStruct(unsigned long id, byte len, byte* data) {
-  SERIAL.print("{.time=");
-  SERIAL.print(millis());
-  SERIAL.print(", .id=0x");
-  SERIAL.print(id, 16);
-  SERIAL.print(", .len=");
-  SERIAL.print(len);
-  SERIAL.print(", .data={");
-  for (int i = 0; i < len; ++i) {
-    if (i != 0) SERIAL.print(", ");
-    SERIAL.print("0x");
-    SERIAL.print(data[i], 16);
-  }
-  SERIAL.print("}},\n");
-}
-
-
-void printSimply(unsigned long id, byte len, byte* data) {
-  SERIAL.print(id);
-  SERIAL.print(":");
-  for (int i = 0; i < len; ++i) {
-    SERIAL.print(" ");
-    SERIAL.print(data[i], 16);
-  }
-  SERIAL.print("\n");
-}
-
-void receive() {
-  if (CAN.checkReceive() != CAN_MSGAVAIL) return;
-  byte len = 0;
-  byte data[8];
-  CAN.readMsgBuf(&len, data);
-  unsigned long id = CAN.getCanId();
-
-  if (IS_BASELINE_ID[id]) return;
-  
-  // printAsStruct(id, len, data);
-  printAsStruct(id, len, data);
-}
-
 unsigned long REPLAY_INDEX = 0;
 
 void replay() {
@@ -528,18 +511,39 @@ void replay() {
   ++REPLAY_INDEX;
 }
 
+frame_t FRAMES[] = {
+  {.time=0, .id=0x350, .len=1, .data={0x40}},
+};
+
+unsigned long LAST_SENT = 0;
+
 void send() {
-  if (millis() % 100 != 0) return;
+  // send every 20 ms
+  if (millis() - LAST_SENT < 20) return;
+  LAST_SENT = millis();
 
   for (int i = 0; i < sizeof(FRAMES) / sizeof(frame_t); i++) {
     CAN.sendMsgBuf(FRAMES[i].id, CAN_STDID, FRAMES[i].len, FRAMES[i].data);
     SERIAL.print("send ");
-    SERIAL.println(i);
+    SERIAL.println(millis());
   }
+}
+
+void setup() {
+  // setupBaseline();
+
+  SERIAL.begin(115200);
+  while (!Serial) {};
+
+  while (CAN_OK != CAN.begin(CAN_83K3BPS, MCP_16MHz)) {
+    SERIAL.println("CAN BUS Shield init fail");
+    delay(1000);
+  }
+  SERIAL.println("CAN BUS Shield init ok!");
 }
 
 void loop() {
   // receive();
-  replay();
-  // send();
+  // replay();
+  send();
 }
