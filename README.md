500eb5c75d102e1be0210da407f461f9cb5e8d0e
Konstantin Weitz <konstantin@financialchoice.com>
2024-04-21 10:38:35 -0700

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# slk
+# SLK 
+
+[Here][1]'s a post explaining how to use this code.
+
+[1]: https://www.slkworld.com/threads/homemade-roof-operation-improvements-for-the-r171.600378
--- slk.ino
+++ slk.ino
@@ -6,9 +6,9 @@
 const int SPI_CS_PIN = 9;
 mcp2515_can CAN(SPI_CS_PIN);
 
-#define START_MILLIS 1000  // Start button push repeating after 1s
-#define END_MILLIS 20000  // End button push repeating after 10s
-#define DELAY_MILLIS 20      // Delay between button push indicator signals is 20ms.
+#define START_MILLIS 1000   // Start button push repeating after 1s
+#define END_MILLIS 30000    // End button push repeating after 30s
+#define DELAY_MILLIS 20     // Delay between button push indicator signals is 20ms.
 
 #define ROOF_CAN_ID 0x350
 #define ROOF_OPEN_COMMAND 0x40
@@ -18,8 +18,12 @@ bool isKnownCommand(byte command) {
   return command == ROOF_OPEN_COMMAND || command == ROOF_CLOSE_COMMAND;
 }
 
-unsigned long mostRecentRoofButtonPushMillis = 0;
-byte mostRecentRoofButtonPushData = 0;
+unsigned long millisSince(unsigned long time) {
+  return millis() - time;
+}
+
+unsigned long lastButtonPushMillis = 0;
+byte lastButtonPushCommand = 0;
 
 void receive() {
   if (CAN.checkReceive() != CAN_MSGAVAIL) return;
@@ -28,14 +32,12 @@ void receive() {
   CAN.readMsgBuf(&len, data);
   unsigned long id = CAN.getCanId();
 
-  if (id != ROOF_CAN_ID) return;
-  if (len != 1) return;
-
+  if (id != ROOF_CAN_ID || len != 1) return;
   byte command = data[0];
   if (!isKnownCommand(command)) return;
 
-  mostRecentRoofButtonPushMillis = millis();
-  mostRecentRoofButtonPushData = command;
+  lastButtonPushMillis = millis();
+  lastButtonPushCommand = command;
   SERIAL.print("Roof button was pushed: 0x");
   SERIAL.println(command, 16);
 }
@@ -43,33 +45,19 @@ void receive() {
 unsigned long lastSent = 0;
 
 void send() {
-  unsigned long now = millis();
-
-  byte command = mostRecentRoofButtonPushData;
+  byte command = lastButtonPushCommand;
   if (!isKnownCommand(command)) return;
-  unsigned long millisSinceButtonPush = now - mostRecentRoofButtonPushMillis;
-  if (millisSinceButtonPush < START_MILLIS) return;
-  if (millisSinceButtonPush > END_MILLIS) return;
-  if (now - lastSent < DELAY_MILLIS) return;
+  if (millisSince(lastButtonPushMillis) < START_MILLIS) return;
+  if (millisSince(lastButtonPushMillis) > END_MILLIS) return;
+  if (millisSince(lastSent) < DELAY_MILLIS) return;
 
   CAN.sendMsgBuf(ROOF_CAN_ID, CAN_STDID, 1, &command);
-  lastSent = now;
+  lastSent = millis();
 
   SERIAL.print("sent: 0x");
   SERIAL.print(command, 16);
   SERIAL.print(" at ");
-  SERIAL.println(now);
-}
-
-void alwaysSendOpen() {
-  if (millis() - lastSent < DELAY_MILLIS) return;
-  if (millis() > 4000) return;
-  
-  lastSent = millis();
-  byte data = ROOF_OPEN_COMMAND;
-  CAN.sendMsgBuf(ROOF_CAN_ID, CAN_STDID, 1, &data);
-  SERIAL.print("sending yeah ");
-  SERIAL.println(millis());
+  SERIAL.println(lastSent);
 }
 
 void setup() {
@@ -80,11 +68,10 @@ void setup() {
     SERIAL.println("CAN BUS Shield init fail");
     delay(1000);
   }
-  SERIAL.println("CAN BUS Shield init ok!");
+  SERIAL.println("Setup ok");
 }
 
 void loop() {
   receive();
   send();
-//  alwaysSendOpen();
 }
